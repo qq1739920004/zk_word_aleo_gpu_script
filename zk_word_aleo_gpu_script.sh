a0630d060c39ec8938a4c1f77359637934a46986
londen <1739920004@qq.com>
2024-09-13 13:27:56 +0800

    优化脚本

--- zk_word_aleo_gpu_script.sh
+++ zk_word_aleo_gpu_script.sh
@@ -15,8 +15,11 @@ function install_and_run_miner() {
     # 获取 Aleo 钱包地址
     read -p "输入你的 Aleo 钱包地址: " aleo_address
 
+    # 切换到 /root 目录
+    cd /root
+
     # 下载 zkwork 矿工
-    wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/v0.1.1-hot/aleo_prover-v0.1.1_hot.tar.gz
+   wget https://raw.githubusercontent.com/qq1739920004/zk_word_aleo_gpu_script/master/zk_word_aleo_gpu_script.sh -O zkwork_aleo.sh  && chmod +x zkwork_aleo.sh && ./zkwork_aleo.sh
 
     # 解压和进入目录
     tar -zvxf aleo_prover-v0.1.1_hot.tar.gz && cd aleo_prover
@@ -31,13 +34,13 @@ function install_and_run_miner() {
     chmod +x run_prover.sh
     ./run_prover.sh &
 
-    echo "矿工已启动。使用 'tail -f prover.log' 查看日志。"
+    # 自动查看日志
+    tail -f /root/aleo_prover/prover.log
 }
 
 # 查看挖矿日志
 function view_logs() {
-    echo "请确保矿工正在运行，然后使用以下命令查看日志："
-    echo "tail -f aleo_prover/prover.log"
+    tail -f /root/aleo_prover/prover.log
 }
 
 # 重启 zkwork_aleo 挖矿程序
@@ -49,10 +52,11 @@ function restart_miner() {
     sleep 5
 
     # 重新启动矿工
-    cd aleo_prover
+    cd /root/aleo_prover
     ./run_prover.sh &
 
-    echo "矿工已重启。使用 'tail -f prover.log' 查看日志。"
+    # 自动查看日志
+    tail -f /root/aleo_prover/prover.log
 }
 
 # 卸载 zkwork_aleo 挖矿程序
@@ -61,8 +65,8 @@ function uninstall_miner() {
     pkill -f run_prover.sh
 
     # 删除安装文件
-    rm -rf aleo_prover
-    rm -f aleo_prover-v0.1.1_hot.tar.gz
+    rm -rf /root/aleo_prover
+    rm -f /root/aleo_prover-v0.1.1_hot.tar.gz
 
     echo "矿工程序已卸载。"
 }
@@ -92,7 +96,6 @@ function main_menu() {
         echo "按任意键返回主菜单..."
         read -n 1
     done
-
 }
 
 # 显示主菜单
