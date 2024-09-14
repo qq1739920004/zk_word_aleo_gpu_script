34d48d27b0a9613a2b5533dc07ff6e03026522dc
londen <1739920004@qq.com>
2024-09-14 14:59:41 +0800

    新增v0.1.2的两个版本

--- zk_word_aleo_gpu_script.sh
+++ zk_word_aleo_gpu_script.sh
@@ -10,6 +10,60 @@ if [ "$(id -u)" != "0" ]; then
     exit 1
 fi
 
+function  install_and_run_miner_pre_v0_1_2(){
+ 
+
+    # 切换到 /root 目录
+    cd /root
+
+    # 下载 zkwork 矿工（标准版 v0.1.2）
+   wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/v0.1.2/aleo_prover-v0.1.2_pre.tar.gz
+    # 获取 Aleo 钱包地址
+    read -p "输入你的 Aleo 钱包地址: " aleo_address
+    # 解压和进入目录
+    tar -zvxf aleo_prover-v0.1.2_pre.tar.gz && cd aleo_prover
+
+    # 更新 Aleo 地址并设置自定义矿工名称
+    read -p "输入自定义矿工名称: " miner_name
+    sed -i "s/^reward_address=.*$/reward_address=$aleo_address/" inner_prover.sh
+    sed -i "s/^custom_name=\".*\"$/custom_name=\"$miner_name\"/" inner_prover.sh
+
+    # 启动矿工
+    chmod +x run_prover.sh
+    ./run_prover.sh &
+    wait  # 等待进程结束
+    # 提示用户矿工已启动
+    # 提示用户矿工已启动
+    echo "矿工已启动！"
+    echo "你可以使用 'tail -f /root/aleo_prover/prover.log' 命令来查看日志。" 
+}
+function install_and_run_miner_v0_1_2(){
+  
+
+    # 切换到 /root 目录
+    cd /root
+
+    # 下载 zkwork 矿工（标准版 v0.1.2）
+    wget https://github.com/6block/zkwork_aleo_gpu_worker/releases/download/v0.1.2/aleo_prover-v0.1.2_stable.tar.gz
+    # 获取 Aleo 钱包地址
+    read -p "输入你的 Aleo 钱包地址: " aleo_address
+    # 解压和进入目录
+    tar -zvxf aleo_prover-v0.1.2_stable.tar.gz && cd aleo_prover
+
+    # 更新 Aleo 地址并设置自定义矿工名称
+    read -p "输入自定义矿工名称: " miner_name
+    sed -i "s/^reward_address=.*$/reward_address=$aleo_address/" inner_prover.sh
+    sed -i "s/^custom_name=\".*\"$/custom_name=\"$miner_name\"/" inner_prover.sh
+
+    # 启动矿工
+    chmod +x run_prover.sh
+    ./run_prover.sh &
+    wait
+
+    # 提示用户矿工已启动
+    echo "矿工已启动！"
+    echo "你可以使用 'tail -f /root/aleo_prover/prover.log' 命令来查看日志。" 
+}
 # 安装并运行 zkwork_aleo 挖矿程序（标准版 v0.1.1）
 function install_and_run_miner_v0_1_1() {
     # 获取 Aleo 钱包地址
@@ -26,8 +80,8 @@ function install_and_run_miner_v0_1_1() {
 
     # 更新 Aleo 地址并设置自定义矿工名称
     read -p "输入自定义矿工名称: " miner_name
-    sed -i "s/^reward_address=.*$/reward_address=$aleo_address/" run_prover.sh
-    sed -i "s/^custom_name=\".*\"$/custom_name=\"$miner_name\"/" run_prover.sh
+    sed -i "s/^reward_address=.*$/reward_address=$aleo_address/" run_prover
+    sed -i "s/^custom_name=\".*\"$/custom_name=\"$miner_name\"/" run_prover
 
     # 启动矿工
     chmod +x run_prover.sh
@@ -36,6 +90,7 @@ function install_and_run_miner_v0_1_1() {
     # 提示用户矿工已启动
     echo "矿工已启动！"
     echo "你可以使用 'tail -f /root/aleo_prover/prover.log' 命令来查看日志。"
+    view_logs
 }
 
 # 安装并运行 zkwork_aleo 挖矿程序（hot 版本 v0.1.1）
@@ -64,6 +119,7 @@ function install_and_run_hot_miner_v0_1_1() {
     # 提示用户矿工已启动
     echo "矿工已启动！"
     echo "你可以使用 'tail -f /root/aleo_prover/prover.log' 命令来查看日志。"
+    view_logs
 }
 
 # 安装并运行 zkwork_aleo 挖矿程序（hot 版本 v0.1.0）
@@ -92,6 +148,7 @@ function install_and_run_hot_miner_v0_1_0() {
     # 提示用户矿工已启动
     echo "矿工已启动！"
     echo "你可以使用 'tail -f /root/aleo_prover/prover.log' 命令来查看日志。"
+    view_logs
 }
 
 # 安装并运行 zkwork_aleo 挖矿程序（标准版 v0.1.0）
@@ -120,6 +177,7 @@ function install_and_run_miner_v0_1_0() {
     # 提示用户矿工已启动
     echo "矿工已启动！"
     echo "你可以使用 'tail -f /root/aleo_prover/prover.log' 命令来查看日志。"
+    view_logs
 }
 
 # 查看挖矿日志
@@ -160,25 +218,32 @@ function main_menu() {
         echo "zk.work社区一键 zkwork_aleo 脚本"
         echo "开发者: $DEVELOPER"
         echo "==========================="
-        echo "1. 安装并运行 zkwork_aleo 挖矿程序（标准版 v0.1.1）"
-        echo "2. 安装并运行 zkwork_aleo 挖矿程序（hot 版本 v0.1.1）"
-        echo "3. 安装并运行 zkwork_aleo 挖矿程序（hot 版本 v0.1.0）"
-        echo "4. 安装并运行 zkwork_aleo 挖矿程序（标准版 v0.1.0）"
-        echo "5. 查看挖矿日志"
-        echo "6. 重启 zkwork_aleo 挖矿程序"
-        echo "7. 卸载 zkwork_aleo 挖矿程序"
-        echo "8. 退出脚本"
-        read -p "请输入选项（1-8）: " OPTION
+        echo "v0.1.2两个版本驱动版本跟cube要求较高，如果太低可能会出现
+        Failed to create GpuPuzzle:Failed to create aleo prover
+note:run with RUST BACKTRACE=1 environment variable to display a backtrace"
+        echo "1. 安装并运行 zkwork_aleo 挖矿程序（v0.1.2-stable)）"
+        echo "2. 安装并运行 zkwork_aleo 挖矿程序（v0.1.2-pre,(cpu好选这个）"
+        echo "3. 安装并运行 zkwork_aleo 挖矿程序（标准版 v0.1.1）"
+        echo "4. 安装并运行 zkwork_aleo 挖矿程序（hot 版本 v0.1.1）"
+        echo "5. 安装并运行 zkwork_aleo 挖矿程序（hot 版本 v0.1.0）"
+        echo "6. 安装并运行 zkwork_aleo 挖矿程序（标准版 v0.1.0）"
+        echo "7. 查看挖矿日志"
+        echo "8. 重启 zkwork_aleo 挖矿程序"
+        echo "9. 卸载 zkwork_aleo 挖矿程序"
+        echo "10. 退出脚本"
+        read -p "请输入选项（1-10）: " OPTION
 
         case $OPTION in
-        1) install_and_run_miner_v0_1_1 ;;
-        2) install_and_run_hot_miner_v0_1_1 ;;
-        3) install_and_run_hot_miner_v0_1_0 ;;
-        4) install_and_run_miner_v0_1_0 ;;
-        5) view_logs ;;
-        6) restart_miner ;;
-        7) uninstall_miner ;;
-        8) exit 0 ;;
+        1) install_and_run_miner_v0_1_2 ;;
+        2) install_and_run_miner_pre_v0_1_2 ;;
+        3) install_and_run_miner_v0_1_1 ;;
+        4) install_and_run_hot_miner_v0_1_1 ;;
+        5) install_and_run_hot_miner_v0_1_0 ;;
+        6) install_and_run_miner_v0_1_0 ;;
+        7) view_logs ;;
+        8) restart_miner ;;
+        9) uninstall_miner ;;
+        10) exit 0 ;;
         *) echo "无效选项。" ;;
         esac
         echo "按任意键返回主菜单..."
